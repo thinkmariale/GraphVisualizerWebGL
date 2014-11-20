65c72a98c7b5ddb106552363189f4e84fccd2974
Maria Alejandra Montenegro <mariale@mariales-mbp.home>
2014-11-20 00:09:18 -0500

    Init commit
    init commit of personal project
    using Three.js

---
+++ Visual/Graph.js
@@ -0,0 +1,162 @@
+/**
+  @author Maria Alejandra Montenegro
+
+  Implementation of a graph structure.
+  Consists of Graph, Nodes and Edges.
+**/
+
+function Graph()
+{
+	this.nodeSet = {};
+	this.nodes = [];
+	this.edges = [];
+	this.layout; //type of graph
+	
+}
+
+function Node(node_id)
+{
+  this.id = node_id;
+  this.nodesTo = [];
+  this.nodesFrom = [];
+  this.position = {};
+  this.data = {};
+}
+
+function Edge(source, target)
+{
+  this.source = source;
+  this.target = target;
+  this.data = {};
+}
+
+//-----
+//Add dynamically to the already defined object a new getter
+Graph.prototype.addNode = function(node) 
+{
+	if(this.nodeSet[node.id] == undefined)
+	{
+		 this.nodeSet[node.id] = node;
+    	this.nodes.push(node);
+		return true;
+	}
+	console.log("dd");
+	return false;
+};
+
+Graph.prototype.getNode = function(node_id) 
+{
+  return this.nodeSet[node_id];
+};
+
+Graph.prototype.addEdge = function(source, target)
+{
+  if(source.addConnectedTo(target) === true) 
+  {
+    var edge = new Edge(source, target);
+    this.edges.push(edge);
+    return true;
+  }
+  return false;
+};
+
+//--------
+//node functions
+Node.prototype.addConnectedTo = function(node)
+{
+  if(this.connectedTo(node) === false)  //same type
+  {
+    this.nodesTo.push(node);
+    return true;
+  }
+  return false;
+};
+
+Node.prototype.connectedTo = function(node)
+{
+  for(var i=0; i < this.nodesTo.length; i++) {
+    var connectedNode = this.nodesTo[i];
+    if(connectedNode.id == node.id) {
+      return true;
+    }
+  }
+  return false;
+};
+
+Node.prototype.draw = function(parent)
+{
+	var geometry = new THREE.SphereGeometry( 5, 15, 15 );
+	var material = new THREE.MeshBasicMaterial( { color: Math.random() * 0xf0ffff, opacity: 0.7  } );
+    var draw_obj = new THREE.Mesh( geometry, material );
+
+	
+	draw_obj.position.x = this.position.x;
+	draw_obj.position.y = this.position.y;
+	draw_obj.position.z = this.position.z;
+	var textOpt = ["10pt", "", ""];
+	if(this.data.title === undefined)
+	{
+		var label = new THREE.Label("temp label", textOpt);	
+	}
+	else{
+		var label = new THREE.Label(this.data.title, textOpt);	
+	}
+	
+	label.position.x = draw_obj.position.x;
+  	label.position.y = draw_obj.position.y - 20;
+	label.position.z = draw_obj.position.z;
+	
+	this.data.label_object = label;
+	//parent.add( node_.data.label_object );
+	
+	draw_obj.name = this.data.title;
+	draw_obj.id   = this.id;
+	this.data.draw_obj  = draw_obj;
+	this.position       = draw_obj.position;
+	
+	parent.add(this.data.draw_obj );
+		
+};
+
+//Edge functions
+Edge.prototype.drawLine = function(parent, geometries)
+{
+	var parameters = parameters || {};
+	
+	material = new THREE.LineBasicMaterial({ color: 0xff0000, opacity: 1, linewidth: 1.5 });
+	var geometry = new THREE.Geometry();
+	geometry.vertices.push(this.source.position);
+	geometry.vertices.push(this.target.position);
+
+	line = new THREE.Line( geometry, material, THREE.LinePieces );
+	line.scale.x = line.scale.y = line.scale.z = 1;
+	line.originalScale = 1;
+	geometries.push(geometry);
+
+	parent.add( line );
+};
+
+Edge.prototype.drawCurve = function(parent, geometries, curveRad)
+{
+	var parameters = parameters || {};
+	var SUBDIVISIONS = 40;
+	if(curveRad == null) 
+		curveRad = -0.1;
+	
+	var mid = new THREE.Vector3();
+	mid.x = (this.target.position.x + this.source.position.x )/2 * curveRad;
+	mid.y = (this.target.position.y + this.source.position.y )/2 * curveRad;
+	mid.z = (this.target.position.z + this.source.position.z )/2 * curveRad;
+
+	material  = new THREE.LineBasicMaterial({ color: 0xff0000, opacity: 1, linewidth: 1.5 });
+	var curve = new THREE.QuadraticBezierCurve3();
+	curve.v0 = new THREE.Vector3(this.source.position.x, this.source.position.y, this.source.position.z);
+	curve.v1 = mid;
+	curve.v2 = new THREE.Vector3(this.target.position.x, this.target.position.y, this.target.position.z);
+	
+	var cp = new THREE.CurvePath();
+    cp.add(curve);	
+	line = new THREE.Line(cp.createPointsGeometry(SUBDIVISIONS), material);
+	
+	parent.add(line);
+};
---
+++ Visual/Layout.js
@@ -0,0 +1,235 @@
+/**
+  @author Maria Alejandra Montenegro
+
+  Implementation of graph layout structure.
+  Consists of 
+  random
+  arc diagram
+  Area Grouping
+  Centralized Burst
+  Centralized Ring
+  Circular Ties
+  Elliptical Impolsion
+  Organic Rhizome
+  Radial Convergence
+  Radial Implosion
+  Ramification
+  Segmental Radical Convergence
+  
+**/
+
+function Layout(graph, parent, options)
+{
+	var options = options || {};
+
+	this.layout = options.layout || "random";
+	this.attraction_multiplier = options.attraction || 5;
+	this.repulsion_multiplier  = options.repulsion  || 0.75;
+	this.numNodesCircumference = options.numCircum  || 50;
+	this.graph  = graph;
+	this.parent = parent;
+	this.width  = options.width  || 200;
+	this.height = options.height || 200;
+	this.depth  = options.depth  || 200;
+	 
+	//function called
+	var curve = false;
+	var isSegmentedRadialCovergence = false;
+	var isRadialCovergence = false;
+	var isRadialImplosion  = false;
+	var callback_positionUpdated = options.positionUpdated;
+
+	var EPSILON = 0.000001;
+	var attraction_constant;
+	var repulsion_constant;
+	var forceConstant;
+	var layout_iterations = 0;
+	var temperature = 0;
+	var nodes_length;
+	var edges_length;
+
+	// performance test
+	var mean_time = 0;
+}
+
+Layout.prototype.createRandom = function(geometries)
+{
+	//set position of new node
+	var area_x = 350;
+	var area_y = 300;
+	var area_z = 100;
+  
+	for(var i = 0; i < this.graph.nodes.length; i++)
+	{
+		var x = Math.random() * area_x - Math.random() * area_x ;// Math.random() * area - Math.random() * area  ;
+		var y = Math.random() * area_y - Math.random() * area_y;// Math.floor(Math.random() * (area + area + 1) - area);
+		var z = Math.random() * area_z - Math.random() * area_z;//area - Math.random() * area  ;
+		this.graph.nodes[i].position.x = x;
+	   	this.graph.nodes[i].position.y = y;
+	   	this.graph.nodes[i].position.z = z; 
+		this.graph.nodes[i].draw(this.parent);
+	}
+
+	//drawing edges as line
+	for(var i = 0; i < this.graph.edges.length; i++)
+	{
+		var e = this.graph.edges[i];
+		if(this.curve)
+			e.drawCurve(this.parent, geometries);
+		else
+			e.drawLine(this.parent, geometries);
+	}
+
+}
+/*Layout.prototype.createArcDiagram()
+{
+	
+}
+*/
+
+
+Layout.prototype.createSegmentedRadialConvergence = function(radius,geometries)
+{
+	var counter = 0;
+	var curRad  = radius;
+	
+	this.isSegmentedRadialCovergence = true;
+	
+	while(counter < this.graph.nodes.length)
+	{
+		
+		var total = this.numNodesCircumference;
+	
+		//make sure we have enough, else justa dd remaining
+		if((counter + total) > this.graph.nodes.length)
+			total = Math.floor(this.graph.nodes.length - counter);
+		
+		//creading current circle
+		this.createCircle(curRad,total,counter);
+		
+		if(this.createRadialImplosion == true)
+			return;
+		counter += total;
+		curRad = curRad * 0.6;
+	}
+	
+	//drawing edges as line
+	for(var i = 0; i < this.graph.edges.length; i++)
+	{
+		var e = this.graph.edges[i];
+		if(this.curve)
+			e.drawCurve(this.parent, geometries);
+		else
+			e.drawLine(this.parent, geometries);
+	}
+
+	this.isSegmentedRadialCovergence = false;
+};
+
+Layout.prototype.createRadialImplosion = function(radius, geometries)
+{	
+	this.isRadialImplosion = true;
+	var n = Math.floor(this.graph.nodes.length/2);
+	this.createSphere(n, radius);
+	
+	for(var i = n; i < this.graph.nodes.length; i++)
+	{
+		var vertex = new THREE.Vector3();
+		do {
+			vertex.x = Math.random() * this.width  - Math.random() * this.width ;
+			vertex.y = Math.random() * this.height - Math.random() * this.height;
+			vertex.z = Math.random() * this.depth  - Math.random() * this.depth;
+			
+		} while(vertex.length() > (radius*radius) );
+		
+		this.graph.nodes[i].position.x = vertex.x;
+	   	this.graph.nodes[i].position.y = vertex.y;
+	   	this.graph.nodes[i].position.z = vertex.z; 
+		this.graph.nodes[i].draw(this.parent);
+	}
+
+	//drawing edges as line
+	for(var i = 0; i < this.graph.edges.length; i++)
+	{
+		var e = this.graph.edges[i];
+		if(this.curve)
+			e.drawCurve(this.parent, geometries);
+		else
+			e.drawLine(this.parent, geometries);
+	}
+
+	
+	this.isRadialImplosion = false;
+};
+
+
+Layout.prototype.createRadialConvergence = function(radius, geometries, curveRad)
+{
+	this.isRadialCovergence = true;
+	
+	this.createCircle(radius, this.graph.nodes.length, 0);
+	
+	//drawing edges as curve
+	for(var i = 0; i < this.graph.edges.length; i++)
+	{
+		var e = this.graph.edges[i];
+		if(this.curve)
+			e.drawCurve(this.parent, geometries);
+		else
+			e.drawLine(this.parent, geometries);
+	}
+
+	this.isRadialCovergence = false;
+};
+
+//--------------
+//functions
+Layout.prototype.inCircle = function(x,y,z,rad)
+{
+	var a = x*x + y*y + z*z;
+	if(a < rad*rad)
+		return true;
+	
+	return false;
+};
+
+Layout.prototype.createCircle = function(radius, total, counter){
+	
+	var curz = Math.random() * this.depth - Math.random() * this.depth;
+	var theta =  Math.PI *2 / total;
+	var angle = 0;
+		
+	for(var n = 0; n < total ; n++)
+	{
+		//if(n%10 == 0)
+		//	curz = Math.random() * this.depth - Math.random() * this.depth;
+		var x = Math.cos(angle) * radius;
+		var y = Math.sin(angle) * radius;
+		var z = curz;
+		
+		this.graph.nodes[counter + n].position.x = x;
+		this.graph.nodes[counter + n].position.y = y;
+		this.graph.nodes[counter + n].position.z = z;
+		this.graph.nodes[counter + n].draw(this.parent);
+		
+		angle +=theta;
+	}	
+};
+
+Layout.prototype.createSphere = function(n, radius)
+{
+	for(var i=0;i< n;i++)
+	{
+		var u  = Math.random();
+	   	var v  = Math.random();
+	   	var theta = 2 * Math.PI * u ;
+	   	var phi   = Math.acos(2 * v - 1);
+	   	this.graph.nodes[i].position.x = (radius * Math.sin(phi) * Math.cos(theta));
+	    this.graph.nodes[i].position.y = (radius * Math.sin(phi) * Math.sin(theta));
+	   	this.graph.nodes[i].position.z = (radius * Math.cos(phi));	
+		this.graph.nodes[i].draw(this.parent);
+	}
+};
+
+
+ 
